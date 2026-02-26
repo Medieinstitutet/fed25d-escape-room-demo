f7ac3734a50ed45f62b08d514805c030f29bda3a
Jenni <jenni@posh.se>
2026-02-26 14:23:38 +0200

    add documentation example

--- src/room1/room1.ts
+++ src/room1/room1.ts
@@ -1,6 +1,12 @@
 import './_room1.scss';
 const room1Container: HTMLDivElement | null = document.querySelector('#room1');
 
+/**
+ * This function will display room 1 when called.
+ *
+ * It will also set up the room with proper
+ * event listeners for buttons, etc. and display the room.
+ */
 export function initRoom1() {
   console.log('Room 1 initialized');
 
@@ -12,6 +18,12 @@ export function initRoom1() {
   completeRoom1Btn?.addEventListener('click', markRoom1Completed);
 }
 
+/**
+ * This function dispatches an event to mark the room as completed.
+ *
+ * It is also responsible for hiding the room altogether when the
+ * room is done.
+ */
 function markRoom1Completed() {
   dispatchEvent(new Event('ROOM_1_COMPLETED'));
 
